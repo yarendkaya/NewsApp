f1d95a506084cff6e4fcfd1c993bc69fa2fda74f
evirgenoguz <evirgenoguz@gmail.com>
2024-09-03 13:18:04 +0300

    Build issue
    - fixed graph syntax
    - added internet connection func body

--- app/src/main/java/com/demirkayayaren/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/demirkayayaren/newsapp/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.demirkayayaren.newsapp.ui
 import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -72,8 +73,18 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository) : Andr
 
     }
 
-    fun internetConnection(context: Context): Boolean {
+    private fun internetConnection(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
 
+        return connectivityManager?.getNetworkCapabilities(connectivityManager.activeNetwork)?.run {
+            when {
+                hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } ?: false
     }
 
     private suspend fun headlinesInternet(countryCode: String) {
